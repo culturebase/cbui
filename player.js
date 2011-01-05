5acd6f344c1ef54e461531ec251514956553f023
Ulf Hermann <ulfhermann@culturebase.org>
2011-01-05 12:48:25 +0000

    add mod_dir and data_path options to base data collector, make case converter accept numbers, allow setting with and height as direct HTML properties in player

--- player.js
+++ player.js
@@ -4,6 +4,8 @@ jQuery.CbWidget.player = jQuery.CbWidget.widget.extend({
       allow_fullscreen : true,
       allow_script_access : 'always',
       embed_source : '/player40/flash/player.swf',
+      width : 550, // width and height are necessary to make it play nice with IE
+      height : 350,
       id : 0
    },
    
@@ -17,7 +19,7 @@ jQuery.CbWidget.player = jQuery.CbWidget.widget.extend({
       var embed = $(document.createElement('embed'))
          .attr('flashvars', "config=%2Fplayer40%2Fapi.php%3Fconfig%3D" + self.options.config + "%26id%3D" + self.options.id + "%26type%3Dconfig%26api_type%3Dxml")
          .attr('allowfullscreen', self.options.allow_fullscreen).attr('allowscriptaccess', self.options.allow_script_access)
-         .attr('src', self.options.embed_source);
+         .attr('src', self.options.embed_source).attr('width', self.options.width).attr('height', self.options.height);
       this.element().append(embed);
       return this.base();
    },
