5a67c2862358460a1a7f1d90d88c4f4cdf426671
Ulf Hermann <ulfhermann@culturebase.org>
2011-01-05 12:48:10 +0000

    first implementation of player widget

---
+++ player.js
@@ -0,0 +1,29 @@
+jQuery.CbWidget.player = jQuery.CbWidget.widget.extend({
+   defaultOptions : {
+      config : '_default',
+      allow_fullscreen : true,
+      allow_script_access : 'always',
+      embed_source : '/player40/flash/player.swf',
+      id : 0
+   },
+   
+   constructor : function(element, options) {
+      this.base(element);
+      this.options = jQuery.extend({}, this.defaultOptions, options);
+   },
+   
+   handleShow : function() {
+      var self = this;
+      var embed = $(document.createElement('embed'))
+         .attr('flashvars', "config=%2Fplayer40%2Fapi.php%3Fconfig%3D" + self.options.config + "%26id%3D" + self.options.id + "%26type%3Dconfig%26api_type%3Dxml")
+         .attr('allowfullscreen', self.options.allow_fullscreen).attr('allowscriptaccess', self.options.allow_script_access)
+         .attr('src', self.options.embed_source);
+      this.element().append(embed);
+      return this.base();
+   },
+   
+   handleHide : function() {
+      this.element().children().remove();
+      return this.base();
+   }
+});
