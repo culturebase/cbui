8e0a23b7613b4013a8e42fd2d4e9283be37affa5
Johannes Wüller <jw@heimat.de>
2011-01-06 10:51:09 +0000

    Add CbUi Loader widget.

---
+++ loader.js
@@ -0,0 +1,26 @@
+jQuery.CbWidget.loader = jQuery.CbWidget.widget.extend({
+   constructor: function (options) {
+      this.options = $.extend({
+         opacity:         0.25,
+         fadeingDuration: 250
+      }, options || {});
+
+      this.element = $(document.createElement('div')).addClass('__CbUiLayer')
+         .addClass('__CbUiLoader');
+   },
+
+   handleShow: function () {
+      this.element.appendTo('body').stop()
+         .fadeTo(this.options.fadingDuration, this.options.opacity);
+
+      return this.base();
+   },
+
+   handleHide: function () {
+      this.element.stop().fadeOut(this.options.fadingDuration, function () {
+         $(this).detach();
+      });
+
+      return this.base();
+   }
+});
