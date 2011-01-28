fffadae96c55a63679014db7c0c93d79943a8574
Johannes Wüller <jw@heimat.de>
2011-01-28 12:36:31 +0000

    Debug.

--- player.js
+++ player.js
@@ -18,7 +18,6 @@ jQuery.CbWidget.player = jQuery.CbWidget.widget.extend({
    },
 
    handleReady : function(options) {
-      if (console) console.log(options);
       this.options = jQuery.extend({}, this.defaultOptions, options);
       this.load(options.id, options.image);
       return this;
