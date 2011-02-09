ac3cbc56a278a661f3f821865e13f92dae4cec13
Alexander Nolis <alex@heimat.de>
2011-02-09 15:56:00 +0000

    slides

--- player.js
+++ player.js
@@ -227,30 +227,34 @@ jQuery.CbWidget.playerSlides = jQuery.CbWidget.widget.extend({
                          <div class="slider-wrap">\
                            <div class="slider"></div>\
                          </div>');
-      $.each(options.slides, function(i, src){
-         var img = $(document.createElement('img')).attr('src', src);
-         // first image of slide is video trigger
-         if(i == 0) {            
-            var icon = jQuery(document.createElement('img')).attr('src', options.play_icon);
-            icon.css({
-                  position:'absolute',
-                  top: ((img.height()/2)-(icon.height()/2)),
-                  left: ((img.width()/2)-(icon.width()/2))
-               }).click(function() {
+      if(options.slides.length >= 1) {
+         $.each(options.slides, function(i, src){
+            var img = $(document.createElement('img')).attr('src', src);
+            // first image of slide is video trigger
+            if(i == 0) {
+               var icon = jQuery(document.createElement('img')).attr('src', options.play_icon);
+               icon.css({
+                     position:'absolute',
+                     top: ((img.height()/2)-(icon.height()/2)),
+                     left: ((img.width()/2)-(icon.width()/2))
+                  }).click(function() {
+                     self.player.play();
+                  });
+               icon.insertBefore(img);
+               img.addClass('video-trigger').click(function() {
                   self.player.play();
                });
-            icon.insertBefore(img);
-            img.addClass('video-trigger').click(function() {
-               self.player.play();
-            });
-         }
-         else {
-            img.click(function() {
-               self.player.load(null, img.attr('src'));
-            });
-         }
-         slideshow.find('.slider').append(img);
-      });
-      self.element().append(slideshow);
+            }
+            else {
+               img.click(function() {
+                  self.player.load(null, img.attr('src'));
+               });
+            }
+            slideshow.find('.slider').append(img);
+         });
+         self.element().append(slideshow);
+      }
+      else
+         self.element().hide();
    }
 });
