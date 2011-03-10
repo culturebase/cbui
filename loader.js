4b02303acec54f888c79b05b284121d348c71d25
Johannes Wüller <jw@heimat.de>
2011-03-10 13:43:48 +0000

    Fix CbUi loader widget.

--- loader.js
+++ loader.js
@@ -1,12 +1,12 @@
 jQuery.CbWidget.loader = jQuery.CbWidget.widget.extend({
    constructor: function (options) {
+      this.base($(document.createElement('div'))
+         .addClass('__CbUiLayer').addClass('__CbUiLoader'));
+
       this.options = $.extend({
          opacity:         0.25,
          fadeingDuration: 250
       }, options || {});
-
-      this.parentElement = $(document.createElement('div'))
-         .addClass('__CbUiLayer').addClass('__CbUiLoader');
    },
 
    handleShow: function () {
